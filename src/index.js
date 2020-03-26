88b523b0d415550b5116dedbcd5a6db5ea6ae27b
lllacc <laccat@qq.com>
2020-03-27 03:43:44 +0800

    引入 jquery

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import _ from 'lodash';
-
+import j from 'jquery'
 
 function component() {
-  let element = document.createElement('div');
+  // let element = document.createElement('div');
+  let element = j('<div></div>')
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello', 'webpack2'], ''))
 
-  return element;
+  // return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
