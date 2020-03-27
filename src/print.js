47410ece9dea38fac24080f87d57d00e7d92e01f
lllacc <laccat@qq.com>
2020-03-27 18:26:24 +0800

    Source Maps

---
+++ src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+  // console.log('I get called from print.js!');
+  cossole.log('I get called from print.js')
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  // entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ],
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
 };
