199ffb1e2b71a25e32c75ae1bf84366b87942cd7
lllacc <laccat@qq.com>
2020-03-29 16:12:06 +0800

    build6

---
+++ build/precache-manifest.838df38c434c50e853af5fbaf0f5ce6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cbb79ee84b52c0d0e34be50e10f2cb",
+    "url": "/project-react-1/build/index.html"
+  },
+  {
+    "revision": "a3164ef82f93b545d65e",
+    "url": "/project-react-1/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6770f6aa8694d38e480",
+    "url": "/project-react-1/build/static/js/2.e364926c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-react-1/build/static/js/2.e364926c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3164ef82f93b545d65e",
+    "url": "/project-react-1/build/static/js/main.2c3dc150.chunk.js"
+  },
+  {
+    "revision": "ad95f7a5769093d8e1be",
+    "url": "/project-react-1/build/static/js/runtime-main.1610158d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-react-1/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.ded2441637521a0b38af605d40258eb2.js"
+  "/project-react-1/build/precache-manifest.838df38c434c50e853af5fbaf0f5ce6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-react-1/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
