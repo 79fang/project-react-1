b032905f1899cd7301025982bb04bc3368b73f9b
lllacc <laccat@qq.com>
2020-03-31 10:49:55 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -10,26 +10,31 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        { id: 1, title: '第一个待办' },
-        { id: 2, title: '第二个待办' },
+
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
 
-        </li>)
+        </li>
+      )
     })
+
+
+
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo}
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
 
         </div>
         <ol>
@@ -38,7 +43,36 @@ class App extends Component {
       </div>
     )
   }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+
+  }
+
+
+
 }
 
 
 export default App;
+
+let id = 0
+
+function idMaker() {
+  id += 1
+  return id
+}
